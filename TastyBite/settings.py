aefe8e1fad516240233a8feb38c758822b5e2973
MDAsrafulhasan <soumick940@gmail.com>
2024-09-27 21:20:14 +0600

    fetch problem done

--- TastyBite/settings.py
+++ TastyBite/settings.py
@@ -49,6 +49,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'rest_framework',
     'django_filters',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -59,6 +60,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+   "http://127.0.0.1:5500"
 ]
 
 ROOT_URLCONF = 'TastyBite.urls'
