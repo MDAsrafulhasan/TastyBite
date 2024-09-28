fb16c43334eb6b5419296b565ff8607e3bdeb3f7
MDAsrafulhasan <soumick940@gmail.com>
2024-09-29 05:00:17 +0600

    user data fetch resolve

--- TastyBite/settings.py
+++ TastyBite/settings.py
@@ -63,9 +63,21 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
+
+
+
+
 CORS_ALLOWED_ORIGINS = [
-   "http://127.0.0.1:5500"
+   "http://127.0.0.1:5500",
+
+
+   
+   "https://tastybite.onrender.com",
 ]
+CORS_ALLOW_CREDENTIALS = True
+
+
+
 
 ROOT_URLCONF = 'TastyBite.urls'
 
