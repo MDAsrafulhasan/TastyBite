8779eb92a5f8a1a78857dff8cfe9611bd42a82b2
MDAsrafulhasan <soumick940@gmail.com>
2024-09-29 22:54:26 +0600

    cors-headers

--- TastyBite/settings.py
+++ TastyBite/settings.py
@@ -63,9 +63,9 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
 ]
 CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = [
-   "http://127.0.0.1:5500"
-]  
+# CORS_ALLOWED_ORIGINS = [
+#    "http://127.0.0.1:5500"
+# ]
 
 
 ROOT_URLCONF = 'TastyBite.urls'
